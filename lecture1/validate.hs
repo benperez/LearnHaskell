fa179f87994513a16f1eb933984e5380f62d5262
Stankalank <ben.prez@gmail.com>
2014-07-07 00:14:10 -0700

    Wrote a point free version

--- lecture1/validate.hs
+++ lecture1/validate.hs
@@ -1,23 +1,36 @@
 import Data.Char
 
+-- Convert a number to its component digits
+-- toDigits 1234 => [1,2,3,4]
+-- toDigits 0 = []
 toDigits :: Integer -> [Integer]
-toDigits n
-  | n > 0     = map (toInteger . digitToInt) (show n)
-  | otherwise = []
+toDigits n 
+  | n > 0  = map (toInteger . digitToInt) (show n)
+  | otherwise [] 
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev n = reverse (toDigits n)
+toDigitsRev = reverse . toDigits
 
-doubleHelper :: [Integer] -> [Integer]
-doubleHelper []       = []
-doubleHelper [x]      = [x]
-doubleHelper (x:y:xs) = [x,y*2] ++ doubleHelper xs
-
+-- Doubles every other integer, from the right
+-- doubleEveryOther [8,7,6,5] == [16,7,12,5]
+-- doubleEveryOther [1,2,3] == [1,4,3]
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther l = reverse ( doubleHelper ( reverse l ) )
+doubleEveryOther = reverse
+                 . zipWith (*) (cycle [1,2])
+                 . reverse
 
+-- Calculate the sum of all digits of the given numbers
+-- sumDigits [16,7,12,5] = 1 + 6 + 7 + 1 + 2 + 5 = 22
 sumDigits :: [Integer] -> Integer
-sumDigits l = sum (concatMap toDigits l)
+sumDigits = sum 
+          . concatMap toDigits 
 
+-- Whether or not an Integer would be a valid credit card no.
+-- validate 4012888888881881 = True
+validate 4012888888881882 = False
 validate :: Integer -> Bool
-validate n = sumDigits ( doubleEveryOther (toDigits n) ) `mod` 10 == 0
+validate = (==) 0 
+         . flip mod 10
+         . sumDigits
+         . doubleEveryOther
+         . toDigits
