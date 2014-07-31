87a4b93ac4087b71248c8e542b3b5bf0f6b739d0
Stankalank <ben.prez@gmail.com>
2014-07-30 20:47:37 -0700

    Solved skips

--- lecture3/Golf.hs
+++ lecture3/Golf.hs
@@ -1,5 +1,24 @@
 module Golf where
+import Control.Category ((>>>))
 
+-- skips "ABCD"  => ["ABCD","BD","C","D"]
+-- skips "hello" => ["hello!","el!","l!","l","o","!"]
+-- skips [True, False] => [[True, False], [False]]
 skips :: [a] -> [[a]]
-skips n  = concat
-       . map (map head . takeWhile (not . null) . iterate (drop n))
+--skips l = map (\n -> everyn n l) [1..(length l)]
+
+everyn :: Int -> [a] -> [a]
+everyn n = map head .
+               takeWhile (not . null) .
+               iterate (drop n) .
+               drop (n - 1)
+
+-- alternatively
+everyn' :: Int -> [a] -> [a]
+everyn' n = drop (n - 1) >>>
+            iterate (drop n) >>>
+            takeWhile (not . null) >>>
+            map head
+
+--localMaxima :: [Integer] -> [Integer]
+--localMaxima l = map fst ( filter (\xs -> ((fst xs) == (foldl1 max xs))) (zip3 l l l) )
