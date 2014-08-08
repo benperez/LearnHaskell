d50c448c5618f87438041410edf163a5ba600f47
Ben Perez <ben.prez@gmail.com>
2014-08-08 10:37:35 -0700

    Solved local maximum

--- lecture3/Golf.hs
+++ lecture3/Golf.hs
@@ -1,24 +1,22 @@
 module Golf where
-import Control.Category ((>>>))
 
 -- skips "ABCD"  => ["ABCD","BD","C","D"]
 -- skips "hello" => ["hello!","el!","l!","l","o","!"]
 -- skips [True, False] => [[True, False], [False]]
 skips :: [a] -> [[a]]
 skips l = map (\n -> everyn n l) [1..(length l)]
+    where everyn n = map head .
+                         takeWhile (not . null) .
+                         iterate (drop n) .
+                         drop (n - 1)
 
-everyn :: Int -> [a] -> [a]
-everyn n = map head .
-               takeWhile (not . null) .
-               iterate (drop n) .
-               drop (n - 1)
-
--- alternatively
-everyn' :: Int -> [a] -> [a]
-everyn' n = drop (n - 1) >>>
-            iterate (drop n) >>>
-            takeWhile (not . null) >>>
-            map head
-
---localMaxima :: [Integer] -> [Integer]
---localMaxima l = map fst ( filter (\xs -> ((fst xs) == (foldl1 max xs))) (zip3 l l l) )
+--localMaxima [2,9,5,6,1] == [9,6]
+--localMaxima [2,3,4,1,5] == [4]
+--localMaxima [1,2,3,4,5] == []
+localMaxima :: [Integer] -> [Integer]
+localMaxima l = map fst $ filter snd $ zip l both
+    where left = [(+ 1) . head $ l] ++ init l
+          right = tail l ++ [(+ 1) . last $ l]
+          gtleft = zipWith (>) l left
+          gtRight = zipWith (>) l right
+          both = zipWith (&&) gtleft gtRight
