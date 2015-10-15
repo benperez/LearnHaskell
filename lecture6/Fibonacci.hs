53df242459564b9e27d5e7a8ca9443c504a4f868
Ben Perez <ben.prez@gmail.com>
2015-10-15 11:21:32 -0700

    added tail recursive linear time fibonacci sequence

--- lecture6/Fibonacci.hs
+++ lecture6/Fibonacci.hs
@@ -8,5 +8,12 @@ fib n = fib (n - 1) + fib (n - 2)
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
+fib2 :: Integer -> Integer
+fib2 n = go n (0, 1)
+  where
+    go n (a, b)
+      | n == 0    = a
+      | otherwise = go (n - 1) (b, a + b)
+
 fibs2 :: [Integer]
 fibs2 = map fromIntegral $ unfoldr (\(a,b) -> Just (a,(b,a+b))) (0,1)
