3ed7104a5c8e7287961d1fed453a3d5d4a991610
Ben Perez <ben.prez@gmail.com>
2015-10-15 13:45:53 -0700

    use bang patterns

--- lecture6/Fibonacci.hs
+++ lecture6/Fibonacci.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 import Data.List (unfoldr)
 
 fib :: Integer -> Integer
@@ -11,9 +13,9 @@ fibs1 = map fib [0..]
 fib2 :: Integer -> Integer
 fib2 n = go n (0, 1)
   where
-    go n (a, b)
+    go !n (!a, !b)
       | n == 0    = a
       | otherwise = go (n - 1) (b, a + b)
 
 fibs2 :: [Integer]
-fibs2 = map fromIntegral $ unfoldr (\(a,b) -> Just (a,(b,a+b))) (0,1)
+fibs2 = map fromIntegral $ unfoldr (\(!a,!b) -> Just (a,(b,a+b))) (0,1)
