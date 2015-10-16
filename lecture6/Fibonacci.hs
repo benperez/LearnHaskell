b0a091e78fd1baa49f4489264d05c95742207954
Ben Perez <ben.prez@gmail.com>
2015-10-15 22:51:10 -0700

    stream functionality

--- lecture6/Fibonacci.hs
+++ lecture6/Fibonacci.hs
@@ -2,6 +2,8 @@
 
 import Data.List (unfoldr)
 
+-- Naive recursive (exponential) fibonacci
+
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -9,6 +11,8 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+-- Tail recursive (linear) fibonacci
 
 fib2 :: Integer -> Integer
 fib2 n = go n (0, 1)
@@ -19,3 +23,37 @@ fib2 n = go n (0, 1)
 
 fibs2 :: [Integer]
 fibs2 = map fromIntegral $ unfoldr (\(!a,!b) -> Just (a,(b,a+b))) (0,1)
+
+-- Stream base functionality
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
+
+-- Stream implementations
+
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 0
+
+--0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, ...
+--0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, ...
+ruler :: Stream Integer
+ruler = go 0
+  where
+    go n = interleaveStreams (streamRepeat n) (go (n + 1))
