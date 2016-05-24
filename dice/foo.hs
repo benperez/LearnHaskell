9c3e676322b49564522d9ca19a599c335875d128
Benjamin Perez <ben.prez@gmail.com>
2016-05-24 15:44:58 -0700

    use monoid

--- dice/foo.hs
+++ dice/foo.hs
@@ -1,4 +1,5 @@
 import Data.List (group, reverse, sort, transpose)
+import Data.Monoid
 
 allUnique :: String -> Bool
 allUnique = any ((>1) . length) . group . sort
@@ -13,13 +14,18 @@ escapeSpaces (x:xs)
   | otherwise = x:(escapeSpaces xs)
 
 numberToSound :: Int -> String
-numberToSound n | div3 && div5 = "fizzbuzz"
-                | div3         = "fizz"
-                | div5         = "buzz"
-                | otherwise    = show n
+numberToSound n
+  | not (div3 || div5) = show n
+  | otherwise          = mappend (if div3 then "fizz" else mempty)
+                                 (if div5 then "buzz" else mempty)
   where
     div3 = n `mod` 3 == 0
     div5 = n `mod` 5 == 0
 
 main :: IO ()
 main = mapM_ (putStrLn . numberToSound) [1..15]
+
+myreverse :: [a] -> [a]
+myreverse l = rev l []
+  where rev [] a       = a
+        rev (x:xs) acc = rev xs (x:acc)
